b57f3d3fe119b5ad04096d261f630ca096b7d83b
Benjamin203 <Benjamin.hoefele@gmail.com>
2020-07-02 22:00:59 +0200

    ui

---
+++ Javascript.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Javascript</title>
+    </head>
+    <body>
+        <script>
+            for (i=0;i<5;i++) {
+                document.write("this is iteration" + i + "<br>")
+            }
+        </script>
+    </body>
+</html>
